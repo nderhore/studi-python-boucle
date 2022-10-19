4c169132c081b826273a69f61a6e7144ac6d7143
Nathan Derhore <nathan@derhore.fr>
2022-10-19 13:03:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# On vous donne une list n, parcourez cette liste et affichez ses valeurs
+def display_list(ma_liste: list):
+    print("TODO")
+
+
+# Completez la fonction afin qu'elle puisse nous afficher les 100 premiers nombres entiers
+def display_hundred_int():
+    print("TODO")
+
+
+# Demandez à l'utilisateur des notes, ajoutez les dans un tableau et faites la moyenne des notes.
+# Si la note, est en dessous de 10 (exclu), affichez "Non admin", sinon, "admin"
+def mention_moyenne():
+    print("TODO")
+
+
+# Affichez le nombre de voyelle que comporte un mot saisie par l'utilisateur
+def display_number_voyelle():
+    print("TODO")
+
+
+# Affichez la table de multiplication (jusque 10 inclus) d'un nombre saisie par l'utilisateur
+# different de 0
+def table_multiplication():
+    print("TODO")
+
+
+# Calculez la factorielle d'un nombre
+# Pour rappel, la factorielle de 5 vaux (1*2*3*4*5)
+def factorielle():
+    print("TODO")
